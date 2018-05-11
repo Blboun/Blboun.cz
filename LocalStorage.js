0d7c60b5445526b735a7344727bbcf1030a9f73f
Blboun <39161376+Blboun@users.noreply.github.com>
2018-05-11 20:01:29 +0200

    Update LocalStorage.js

--- LocalStorage.js
+++ LocalStorage.js
@@ -1,5 +1,7 @@
 if (window.localStorage) {
-console.log("Local sotrage was find");
-};
 localStorage.setItem("Numbers of newsapper", "1");
 localStorage.setItem("Year of newspapers", "1");
+console.log("Local sotrage was find");
+} else {
+console.log("Local Storage was not find")
+};
